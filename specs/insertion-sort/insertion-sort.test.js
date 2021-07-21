cc2c32a6f094914a47026f1568faec87786e2ba6
or-halimi <or@visto.li>
2021-07-21 20:14:36 +0300

    insertion sort

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -13,13 +13,35 @@
   And you put xdescribe instead of describe if you want to suspend running the unit tests.  
 */
 
+function insertionSort1(nums) {
+  for(let i = 1; i < nums.length; i++){
+    for(let j=i; j>0; j--){
+      if(nums[j] < nums[j-1]){
+        const swap = nums[j];
+        nums[j] = nums[j-1];
+        nums[j-1] = swap;
+      }
+      else break;
+    }
+  }
+  return nums
+}
+
 function insertionSort(nums) {
-  // code goes here
+  for (let i = 1; i < nums.length; i++) {
+    const numToInsert = nums[i];
+    let j;
+    for (j = i - 1; j >= 0 && nums[j] > numToInsert; j--) {
+      nums[j + 1] = nums[j];
+    }
+    nums[j + 1] = numToInsert;
+  }
+  return nums
 }
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test("insertion sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
--- src/Sort.js
+++ src/Sort.js
@@ -10,7 +10,23 @@ function sort(array) {
   // call snapshot any time you do anything to the array
   // it's okay if you call it with duplicate value array,
   // it will deduplicate for you
-  snapshot(array);
+  let swapHappened = false;
+  let iteractions = 0
+  do{
+    swapHappened = false;
+    for(let i=1; i < nums.length - iteractions; i++){
+    snapshot(array);
+      if (nums[i-1] > nums[i] ){
+        swapHappened = true;
+        const swapper = nums[i-1];
+        nums[i-1] = nums[i];
+        nums[i] = swapper;
+      }
+    }
+    iteractions +=1;
+  } while(swapHappened)
+
+  return nums;
 }
 
 export default function SortComponent() {
