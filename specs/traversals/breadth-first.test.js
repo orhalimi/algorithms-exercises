6225c2b26505a83a1178bd9b6be8cf5cb2a5c368
or-halimi <or@visto.li>
2021-08-17 10:36:56 +0300

    heap and breadth first

--- specs/traversals/breadth-first.test.js
+++ specs/traversals/breadth-first.test.js
@@ -1,10 +1,17 @@
 const breadthFirstTraverse = (queue, array) => {
-  // fill code in here
+
+  while(queue.length > 0){
+    let node = queue.shift();
+    array.push(node.value)
+    if(node.left) queue.push(node.left);
+    if(node.right) queue.push(node.right);
+  }
+  return array;
 };
 
 // unit tests
 // do not modify the below code
-describe.skip("breadth-first tree traversal", function () {
+describe("breadth-first tree traversal", function () {
   const answer = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
 
   const tree = {
